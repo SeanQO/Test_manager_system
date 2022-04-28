21c6d33abce2cbca6c4903890dbaeeb1fc271a9d
SeanQO <seanquintero3@gmail.com>
2022-04-28 14:53:15 -0500

    added spa to login and register forms

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,36 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
+let state = {
+  uname : "",
+  psw: ""
+}
+
+let handleChange = e =>{
+  switch(e.target.name){
+      case "uname": state.uname = e.target.value; 
+        break;
+
+      case "psw": state.psw = e.target.value;
+  }
+}
+
+let handleSubmit = async e => {
+  e.preventDefault();
+  let config = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json' 
+      },
+      body: JSON.stringify(state)
+  }
+
+  let r = await fetch ("http://localhost:3000/login",config)
+
+  console.log(r);
+}
+
 export default function Login() {
   return (
     <div className={styles.container}>
@@ -13,24 +43,24 @@ export default function Login() {
       </Head>
       
       <main className={styles.main}>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>user name:</label>
-                <input type="text" id="uname" name="uname"></input>
+                <input type="text" id="uname" name="uname" onChange={handleChange}></input>
             </div>
             <div>
                 <label>password:</label>
-                <input type="text" id="psw" name="psw"></input>
+                <input type="text" id="psw" name="psw" onChange={handleChange}></input>
             </div>
             <div>
-                <Link href="/tests">
                   <button type="primary">login</button>
-                </Link>
             </div>
             <div>
                 <Link href="/register">
                   <a type="primary">register</a>
                 </Link>
             </div>
+          </form>
       </main>
 
     </div>
--- src/pages/register.js
+++ src/pages/register.js
@@ -2,6 +2,40 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+let state = {
+  uname : "",
+  psw: "",
+  type: ""
+}
+
+let handleChange = e =>{
+  switch(e.target.name){
+    case "utype": state.type = e.target.value; 
+      break;  
+    
+    case "uname": state.uname = e.target.value; 
+      break;
+
+    case "psw": state.psw = e.target.value;
+
+  }
+}
+
+let handleSubmit = async e => {
+  e.preventDefault();
+  let config = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json' 
+      },
+      body: JSON.stringify(state)
+  }
+
+  let r = await fetch ("http://localhost:3000/register",config)
+
+  console.log(r);
+}
 
 export default function Register() {
   return (
@@ -13,27 +47,25 @@ export default function Register() {
       </Head>
       
       <main className={styles.main}>
+        <form onSubmit={handleSubmit}>
             <div>
-              <form>
-                <input type="radio" id="tchr" name="utype" value="Teacher"></input>
-                <label for="tchr">Teacher</label><br></br>
-                <input type="radio" id="stud" name="utype" value="Student"></input>
-                <label for="stud">Student</label><br></br>
-              </form>
+                <input type="radio" id="tchr" name="utype" value="teacher" onClick={handleChange}></input>
+                <label htmlFor="tchr">Teacher</label><br></br>
+                <input type="radio" id="stud" name="utype" value="student" onClick={handleChange}></input>
+                <label htmlFor="stud">Student</label><br></br>
             </div>
             <div>
                 <label>user:</label>
-                <input type="text" id="uname" name="uname"></input>
+                <input type="text" id="uname" name="uname" onChange={handleChange}></input>
             </div>
             <div>
                 <label>password:</label>
-                <input type="text" id="psw" name="psw"></input>
+                <input type="text" id="psw" name="psw" onChange={handleChange}></input>
             </div>
             <div>
-                <Link href="/login">
                   <button type="primary">register</button>
-                </Link>
             </div>
+        </form>
       </main>
 
     </div>
