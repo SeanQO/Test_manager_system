9600622c9f8fe2257d2815c2d2a3e3d440b8ea88
SeanQO <seanquintero3@gmail.com>
2022-04-28 01:52:44 -0500

    created basic login and register templates, added empty components and routes to teachers, students and exmans

---
+++ pages/login.js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>login</title>
+        <meta name="description"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <main className={styles.main}>
+            <div>
+                <label>user name:</label>
+                <input type="text" id="uname" name="uname"></input>
+            </div>
+            <div>
+                <label>password:</label>
+                <input type="text" id="psw" name="psw"></input>
+            </div>
+            <div>
+                <Link href="/register">
+                  <button type="primary">login</button>
+                </Link>
+            </div>
+            <div>
+                <Link href="/register">
+                  <a type="primary">register</a>
+                </Link>
+            </div>
+      </main>
+
+    </div>
+  )
+
+}
---
+++ pages/register.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>register</title>
+        <meta name="description"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <main className={styles.main}>
+            <div>
+              <form>
+                <input type="radio" id="tchr" name="utype" value="Teacher"></input>
+                <label for="tchr">Teacher</label><br></br>
+                <input type="radio" id="stud" name="utype" value="Student"></input>
+                <label for="stud">Student</label><br></br>
+              </form>
+            </div>
+            <div>
+                <label>user:</label>
+                <input type="text" id="uname" name="uname"></input>
+            </div>
+            <div>
+                <label>password:</label>
+                <input type="text" id="psw" name="psw"></input>
+            </div>
+            <div>
+                <Link href="/login">
+                  <button type="primary">register</button>
+                </Link>
+            </div>
+      </main>
+
+    </div>
+  )
+
+}
